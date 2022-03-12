e4a9e51255a60eded9f1dd35d25b58c01e69a3dc
Travis Roberts <27257343+tsrob50@users.noreply.github.com>
2022-03-12 14:23:53 -0600

    Add description and notes

--- SysprepCSE.ps1
+++ SysprepCSE.ps1
@@ -1,9 +1,33 @@
+
+<#
+.SYNOPSIS
+    Used as a custom script extension for running Sysprep.ext on Windows VM's in Azure
+.DESCRIPTION
+    This Custom Script Extension is used to run Sysprep on a VM to prepare it for imaging.
+    /mode:vm is used to speed up first boot on VM's by skipping hardware detection.
+    Remove "/mode:vm" if the image will be deployed to different VM types then the source VM.
+    More info here: https://www.ciraltos.com/please-wait-for-the-windows-modules-installer/
+
+.NOTES
+    ## Script is offered as-is with no warranty, expressed or implied.  ##
+    ## Test it before you trust it!                                     ##
+    Author      : Travis Roberts, Ciraltos llc
+    Website     : www.ciraltos.com
+    Version     : 1.0.0.0 Initial Build 3/12/2022
+
+.LINK
+
+#>
+
+
 #Script to run Sysprep on a VM
+#Logging is handy when you need it!
 if ((test-path c:\logfiles) -eq $false) {
     new-item -ItemType Directory -path 'c:\' -name 'logfiles' | Out-Null
 } 
 $logFile = "c:\logfiles\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
 
+# Logging function
 function Write-Log {
     Param($message)
     Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
@@ -11,7 +35,7 @@ function Write-Log {
 #Run Sysprep
 try{
     write-output "Sysprep Starting"
-    Start-Process -filepath 'c:\Windows\system32\sysprep\sysprep.exe' -Wait -ErrorAction Stop -ArgumentList  '/shutdown', '/oobe', '/mode:vm', '/quiet'
+    #Start-Process -filepath 'c:\Windows\system32\sysprep\sysprep.exe' -Wait -ErrorAction Stop -ArgumentList  '/shutdown', '/oobe', '/mode:vm', '/quiet'
     write-output "Sysprep Started"
 }
 catch {
